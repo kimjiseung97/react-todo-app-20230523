ba6002178693c8d43e2e8f75386787f9474ebb60
jisuengkim <dickseung23@naver.com>
2023-06-09 09:49:45 +0900

    # 회원가입입력값 검증

--- src/component/layout/Header.js
+++ src/component/layout/Header.js
@@ -25,7 +25,7 @@ const Header = () => {
                <Grid item>
                     <div className='btn-group'>
                         <Link to='login'>로그인</Link>
-                        <Link to='join'>로그인</Link>
+                        <Link to='join'>회원가입</Link>
                     </div>
                </Grid>
             </Grid>
